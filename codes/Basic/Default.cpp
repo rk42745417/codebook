f6e1b4e50669bd04acf25c2a0339018d005d48d7
Ji Kuai <rk427454171@gmail.com>
2023-10-18 15:22:19 +0800

    Add more infra

--- codes/Basic/Default.cpp
+++ codes/Basic/Default.cpp
@@ -5,23 +5,6 @@ using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 using uint = unsigned int;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vl = vector<ll>;
-using vvi = vector<vector<int>>;
-using vvl = vector<vector<ll>>;
-#define pb push_back
-#define F first
-#define S second
-#define mid ((LB+RB)/2)
-#define mkp make_pair
-#define iter(x) x.begin(),x.end()
-#define aiter(a,n) a,a+n
-#define REP(n) for (int ___=n > 0 ? n : 0;___--;)
-#define REP0(i,n) for (int i=0,___=n;i<___;++i)
-#define REP1(i,n) for (int i=1,___=n;i<=___;++i)
-#define MEM(e,val) memset (e,val,sizeof(e))
 const double EPS  = 1e-8;
 const int INF = 0x3F3F3F3F;
 const ll LINF = 4611686018427387903;
